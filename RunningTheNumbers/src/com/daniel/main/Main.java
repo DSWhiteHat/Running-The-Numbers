a38109c71b3711a25c9d08987909457bbf8e389b
DSWhiteHat <33768979+DSWhiteHat@users.noreply.github.com>
2020-07-01 22:35:42 -0400

    File storage is hard

--- RunningTheNumbers/src/com/daniel/main/Main.java
+++ RunningTheNumbers/src/com/daniel/main/Main.java
@@ -23,218 +23,187 @@ public class Main
 	public static void main(String[] args) throws IOException
 	{
 		// PApplet.main("com.daniel.main.RunningTheNumbers");
+
+		/*
+		File test = new File("Data/Boys/2011/Hempfield @ Cocalico.csv");
+		scrub(test);
+		BufferedReader reader = new BufferedReader(new FileReader(test));
+
+		String line = reader.readLine();
+		while (line != null)
+		{
+			System.out.println(line);
+			line = reader.readLine();
+		}
+		*/
 		
-		File performances = new File("Data/performances.csv");
-		scrub(performances);
+		String test = "test,  test ,lalala lalala, blah  ";
+		String[] split = separate(test);
+		for(int i = 0; i < split.length; i++)
+		{
+			System.out.print(split[i] + ",");
+		}
 		
 		/*
-		File meets = new File("Data/meets.csv");
-		scrub(meets);
-
-		//System.out.println(query(performances, meets, "McCaskey & Annville-Cleona @ Lancaster Catholic (Boys)", "9/7/2010"));
-		System.out.println(query(performances, meets, "1600 Time Trial (Boys)", "2010"));
-		*/
+		 * File meets = new File("Data/meets.csv"); scrub(meets); //System.out.println(query(performances, meets,
+		 * "McCaskey & Annville-Cleona @ Lancaster Catholic (Boys)", "9/7/2010")); System.out.println(query(performances, meets,
+		 * "1600 Time Trial (Boys)", "2010"));
+		 */
 	}
 
-	//Cleans up the csv lines to prepare them for analysis.
+	// Cleans up the csv lines to prepare them for analysis.
 	public static void scrub(File file) throws IOException
 	{
 		BufferedReader reader = new BufferedReader(new FileReader(file));
 		List<String> lines = new ArrayList<String>();
-		boolean quotes = false;
 
+		// Meet Title.
 		String line = reader.readLine();
-		while (line != null)
+		lines.add(line.substring(0, line.length() - 9));
+
+		// Date.
+		line = reader.readLine();
+		lines.add(line.substring(0, line.length() - 9));
+
+		// Conditions.
+		line = reader.readLine();
+		line = line.substring(12, line.length() - 8);
+		if (line.charAt(0) == '"')
 		{
-			//Removing header row to leave only relevant statistics in the file.
-			if (!(line.contains("Name") && line.contains("Date")))
-			{
-				//Removing leading spaces.
-				while (line.charAt(0) == ' ')
-				{
-					line = line.substring(1);
-				}
-
-				for (int i = 1; i < line.length(); i++)
-				{
-					//Leaving sections in quotes untouched.
-					if (line.charAt(i) == '"')
-					{
-						quotes = !quotes;
-					}
-
-					//Removing extra spaces next to commas or other spaces.
-					if (!quotes && line.charAt(i) == ' ' && (line.charAt(i + 1) == ',' || line.charAt(i - 1) == ',' || line.charAt(i + 1) == ' '))
-					{
-						line = line.substring(0, i) + line.substring(i + 1);
-					}
-					
-					//Removing extra spaces next to commas or other spaces.
-					if (!quotes && line.charAt(i) == ':' && (line.charAt(i + 3) == ':'))
-					{
-						line = line.substring(0, i + 3) + line.substring(i + 6);
-					}
-				}
-
-				lines.add(line);
-			}
-
-			line = reader.readLine();
+			line = line.substring(1, line.length() - 1);
 		}
+		lines.add(line);
 
-		reader.close();
-
-		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-
-		//Putting the lines back in the file (except the header row).
-		for (String l : lines)
-		{
-			writer.write(l + "\n");
-		}
-
-		writer.close();
-	}
-
-	//Returns the meet specified.
-	public static Meet query(File performances, File meets, String name, String date) throws IOException
-	{
-		String search = name.toLowerCase();
-
-		BufferedReader reader = new BufferedReader(new FileReader(meets));
-
-		//Finding line with meet info from the "Meets" file.
-		String line = reader.readLine();
-		while (line != null && !(line.toLowerCase().contains(search) && line.contains(date)))
-		{
-			line = reader.readLine();
-		}
-
-		//If no meet matched search criteria.
-		if (line == null)
-		{
-			System.out.println("Search Failed.");
-			reader.close();
-			return null;
-		}
-		
-		//Removing unnecessary commas at the end of the line, places for extra score reports that were not used in this meet.
-		while (line.charAt(line.length() - 1) == ',')
-		{
-			line = line.substring(0, line.length() - 1);
-		}
+		reader.readLine();
+		line = reader.readLine();
 
 		String[] split = separate(line);
-
-		List<Scores> results = new ArrayList<Scores>();
-
-		//Separating the team names and scores from the end of the line.
-		for (int i = 4; i < split.length; i++)
+		List<Integer> goodIndices = new ArrayList<Integer>();
+		for (int i = 0; i < split.length; i++)
 		{
-			List<String> teamNames = new ArrayList<String>();
-			List<Integer> scores = new ArrayList<Integer>();
-
-			String[] temp = split[i].split(" - ");
-
-			for (int j = 0; j < temp.length; j++)
+			if (split[i].equals("Mile 1") || split[i].equals("Place") || split[i].equals("Mile 2") || split[i].equals("5k") || split[i].equals("Fin. Place") || split[i].equals("Comments"))
 			{
-				teamNames.add(temp[j].substring(0, temp[j].lastIndexOf(' ')));
-				scores.add(Integer.parseInt(temp[j].substring(temp[j].lastIndexOf(' ') + 1)));
-			}
 
-			results.add(new Scores(teamNames, scores));
+			}
 		}
 
-		//Creating Meet object.
-		Meet output = new Meet(name, new Date(split[1]), split[2], split[3].replaceAll("\"", ""), results);
-
-		reader.close();
-		reader = new BufferedReader(new FileReader(performances));
-
-		//Searching for the lines containing the performances run at this meet.
 		line = reader.readLine();
 		while (line != null)
 		{
-			if (line.toLowerCase().contains(search) && line.contains(date))
-			{
-				String[] performance = separate(line);
 
-				List<Time> times = new ArrayList<Time>();
-				List<Integer> places = new ArrayList<Integer>();
-
-				//Finding all times listed on the line, both splits and final, whatever is provided.
-				for (int i = 4; i <= 8; i += 2)
-				{
-					if (performance[i] != null && !performance[i].equals(""))
-					{
-						times.add(new Time(performance[i]));
-					}
-				}
-
-				//Finding all places listed on the line, both splits and final, whatever is provided.
-				for (int i = 3; i <= 7; i += 2)
-				{
-					if (performance[i] != null && !performance[i].equals(""))
-					{
-						//If someone missed this person's place.
-						if(performance[i].equals("?"))
-						{
-							places.add(-1);
-						}
-						else
-						{
-							places.add(Integer.parseInt(performance[i]));
-						}
-					}
-				}
-				
-				//Adding the place on the team. Codes: -1 -> unknown, -2 -> Did Not Finish, -3 -> Did Not Race.
-				int temp = 0;
-				if(performance[9].equals("?"))
-				{
-					temp = -1;
-				}
-				else if(performance[9].equals("DNF"))
-				{
-					temp = -2;
-				}
-				else if(performance[9].equals("DNR"))
-				{
-					temp = -3;
-				}
-				else
-				{
-					temp = Integer.parseInt(performance[9]);
-				}
-				output.addPerformance(new Run(performance[0], times, output, temp, places));
-			}
 			line = reader.readLine();
 		}
-
-		reader.close();
-		return output;
 	}
 
-	//Spliting a csv line around commas, but ignoring sections in quotes.
+	// Returns the meet specified.
+	public static Meet query(String folder, String gender, String year, String query) throws IOException
+	{
+		File directory = new File(folder + "/" + gender + "/" + year);
+		File[] meets = directory.listFiles();
+		int index = -1;
+
+		for (int i = 0; i < meets.length; i++)
+		{
+			if (meets[i].getName().toLowerCase().contains(query.toLowerCase()))
+			{
+				index = i;
+				break;
+			}
+		}
+
+		if (index == -1)
+		{
+			System.out.println("Meet Not Found");
+		}
+		else
+		{
+			scrub(meets[index]);
+			BufferedReader reader = new BufferedReader(new FileReader(meets[index]));
+
+			String line = reader.readLine();
+			while (line != null)
+			{
+
+			}
+		}
+
+		return null;
+	}
+
+	/*
+	 * public static void scrub(File file) throws IOException { BufferedReader reader = new BufferedReader(new FileReader(file));
+	 * List<String> lines = new ArrayList<String>(); boolean quotes = false; String line = reader.readLine(); while (line != null) {
+	 * //Removing header row to leave only relevant statistics in the file. if (!(line.contains("Name") && line.contains("Date"))) {
+	 * //Removing leading spaces. while (line.charAt(0) == ' ') { line = line.substring(1); } for (int i = 1; i < line.length(); i++)
+	 * { //Leaving sections in quotes untouched. if (line.charAt(i) == '"') { quotes = !quotes; } //Removing extra spaces next to
+	 * commas or other spaces. if (!quotes && line.charAt(i) == ' ' && (line.charAt(i + 1) == ',' || line.charAt(i - 1) == ',' ||
+	 * line.charAt(i + 1) == ' ')) { line = line.substring(0, i) + line.substring(i + 1); } //Removing extra spaces next to commas or
+	 * other spaces. if (!quotes && line.charAt(i) == ':' && (line.charAt(i + 3) == ':')) { line = line.substring(0, i + 3) +
+	 * line.substring(i + 6); } } lines.add(line); } line = reader.readLine(); } reader.close(); BufferedWriter writer = new
+	 * BufferedWriter(new FileWriter(file)); //Putting the lines back in the file (except the header row). for (String l : lines) {
+	 * writer.write(l + "\n"); } writer.close(); }
+	 */
+
+	// Returns the meet specified.
+	/*
+	 * public static Meet query(File performances, File meets, String name, String date) throws IOException { String search =
+	 * name.toLowerCase(); BufferedReader reader = new BufferedReader(new FileReader(meets)); //Finding line with meet info from the
+	 * "Meets" file. String line = reader.readLine(); while (line != null && !(line.toLowerCase().contains(search) &&
+	 * line.contains(date))) { line = reader.readLine(); } //If no meet matched search criteria. if (line == null) {
+	 * System.out.println("Search Failed."); reader.close(); return null; } //Removing unnecessary commas at the end of the line,
+	 * places for extra score reports that were not used in this meet. while (line.charAt(line.length() - 1) == ',') { line =
+	 * line.substring(0, line.length() - 1); } String[] split = separate(line); List<Scores> results = new ArrayList<Scores>();
+	 * //Separating the team names and scores from the end of the line. for (int i = 4; i < split.length; i++) { List<String>
+	 * teamNames = new ArrayList<String>(); List<Integer> scores = new ArrayList<Integer>(); String[] temp = split[i].split(" - ");
+	 * for (int j = 0; j < temp.length; j++) { teamNames.add(temp[j].substring(0, temp[j].lastIndexOf(' ')));
+	 * scores.add(Integer.parseInt(temp[j].substring(temp[j].lastIndexOf(' ') + 1))); } results.add(new Scores(teamNames, scores)); }
+	 * //Creating Meet object. Meet output = new Meet(name, new Date(split[1]), split[2], split[3].replaceAll("\"", ""), results);
+	 * reader.close(); reader = new BufferedReader(new FileReader(performances)); //Searching for the lines containing the
+	 * performances run at this meet. line = reader.readLine(); while (line != null) { if (line.toLowerCase().contains(search) &&
+	 * line.contains(date)) { String[] performance = separate(line); List<Time> times = new ArrayList<Time>(); List<Integer> places =
+	 * new ArrayList<Integer>(); //Finding all times listed on the line, both splits and final, whatever is provided. for (int i = 4;
+	 * i <= 8; i += 2) { if (performance[i] != null && !performance[i].equals("")) { times.add(new Time(performance[i])); } }
+	 * //Finding all places listed on the line, both splits and final, whatever is provided. for (int i = 3; i <= 7; i += 2) { if
+	 * (performance[i] != null && !performance[i].equals("")) { //If someone missed this person's place.
+	 * if(performance[i].equals("?")) { places.add(-1); } else { places.add(Integer.parseInt(performance[i])); } } } //Adding the
+	 * place on the team. Codes: -1 -> unknown, -2 -> Did Not Finish, -3 -> Did Not Race. int temp = 0; if(performance[9].equals("?"))
+	 * { temp = -1; } else if(performance[9].equals("DNF")) { temp = -2; } else if(performance[9].equals("DNR")) { temp = -3; } else {
+	 * temp = Integer.parseInt(performance[9]); } output.addPerformance(new Run(performance[0], times, output, temp, places)); } line
+	 * = reader.readLine(); } reader.close(); return output; }
+	 */
+
+	// Splits a CSV line around commas, but ignoring sections in quotes, and removes leading and trailing spaces.
 	public static String[] separate(String csv)
 	{
 		List<String> list = new ArrayList<String>();
 		boolean quote = false;
 		int comma = -1;
 
-		for (int i = 0; i < csv.length(); i++)
+		for (int i = 0; i < csv.length() + 1; i++)
 		{
-			if (csv.charAt(i) == '"')
+			if (i < csv.length() && csv.charAt(i) == '"')
 			{
 				quote = !quote;
 			}
 
-			if (!quote && csv.charAt(i) == ',')
+			if (i == csv.length() || !quote && csv.charAt(i) == ',')
 			{
-				list.add(csv.substring(comma + 1, i));
+				String temp = csv.substring(comma + 1, i);
+				
+				while(temp.charAt(0) == ' ')
+				{
+					temp = temp.substring(1);
+				}
+				
+				while(temp.charAt(temp.length() - 1) == ' ')
+				{
+					temp = temp.substring(0, temp.length() - 1);
+				}
+				
+				list.add(temp);
 				comma = i;
 			}
 		}
-		list.add(csv.substring(comma + 1));
 
 		String[] output = new String[list.size()];
 		output = list.toArray(output);
